9b8300ae85e2b0eaaaa41952bf66e2e631a2eeaf
KNAndrikov18 <KNAndrikov18@codingburgas.bg>
2021-10-15 08:51:48 +0300

    add

---
+++ Classwork23/src/Novel.java
@@ -0,0 +1,22 @@
+
+public class Novel {
+	private int isbn;
+	private String genre;
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+}
